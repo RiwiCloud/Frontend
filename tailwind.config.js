df815a8ee7f88084f0a92a59e7856e6d57dcf524
Johan Arboleda <jhse757@gmail.com>
2024-07-05 20:43:41 -0500

    Crear formulario e interfaz de inicio de sesión.

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      'fuchsia-pink': {
+        '50': '#fef3ff',
+        '100': '#fce6ff',
+        '200': '#f9ccff',
+        '300': '#f7a4ff',
+        '400': '#f56dff',
+        '500': '#ea36ff',
+        '600': '#d216e3',
+        '700': '#b20ebd',
+        '800': '#930e9a',
+        '900': '#7a127d',
+        '950': '#520055',
+      },
+    },
+  },
+  plugins: [
+      require('@tailwindcss/forms'),
+    ],
+  }
+}
